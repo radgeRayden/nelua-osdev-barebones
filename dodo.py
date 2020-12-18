2ab88e2d8d17c395e1f74a2bb02595e0960ea858
Westerbly Snaydley <westerbly@gmail.com>
2020-12-18 01:29:50 -0500

    add launch task to build script

--- dodo.py
+++ dodo.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 cc = "~/.local/opt/cross/bin/i686-elf-gcc"
+qemu = "qemu-system-i386"
 
 def task_build():
     boot_cmd = "nasm -felf32 boot.s -o boot.o"
@@ -9,5 +10,13 @@ def task_build():
     link_cmd = f"{cc} -T linker.ld -o isodir/boot/image.bin -ffreestanding -O2 -nostdlib boot.o kernel.o -lgcc"
     grub_cmd = "grub-mkrescue -o radgeos.iso isodir"
     return {
-        'actions': [boot_cmd, nelua_cmd, gcc_cmd, link_cmd, grub_cmd]
+        'actions': [boot_cmd, nelua_cmd, gcc_cmd, link_cmd, grub_cmd],
+        'targets': ["radgeos.iso"]
+    }
+
+def task_launch():
+    return {
+        'actions': [f"{qemu} -cdrom radgeos.iso"],
+        'file_dep': ["radgeos.iso"],
+        'uptodate': [False]
     }
